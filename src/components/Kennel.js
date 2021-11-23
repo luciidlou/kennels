42ef607c2c04a60683cdbd01a18646e0979b935a
luciidlou <lewisale96@gmail.com>
2021-11-23 14:10:48 -0600

    first commit

---
+++ src/components/Kennel.js
@@ -0,0 +1,10 @@
+import { LocationList } from "./location/LocationList"
+
+export const Kennel = () => {
+    return (
+        <>
+            <h2>Nashville Kennels</h2>
+            <LocationList />
+        </>
+    )
+}
---
+++ src/components/location/LocationList.js
@@ -0,0 +1,10 @@
+import React from "react"
+export const LocationList = () => {
+    return (
+        <>
+            <article className="locations">
+                <h2>List of Locations</h2>
+            </article>
+        </>
+    )
+}
